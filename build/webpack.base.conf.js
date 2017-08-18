4803a6ecaa64895e2d4b30312c63cd9d6ac666a8
Leecason <leecason0722@gmail.com>
2017-08-18 22:02:38 +0800

    Import sass

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -42,6 +42,10 @@ module.exports = {
         options: vueLoaderConfig
       },
       {
+        test: '/\.scss$/',
+        loader: 'style!css!sass'
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test')]
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 import App from './App';
 import router from './router';
+import './stylesheets/index.scss';
 
 Vue.use(ElementUI);
 

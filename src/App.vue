3432ce7b664ef568c916956f2a6f5b77cfe4e477
Leecason <leecason0722@gmail.com>
2017-08-18 22:04:01 +0800

    Add navigation in /home

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="home">
+    <header></header>
+    <div class="sidebar">
+      <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" theme="dark">
+        <el-menu-item index="1">
+          <i class="el-icon-star-on"></i>
+          <span slot="title">导航一</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <div class="main-content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
-<script>
+<script type="javascript">
 export default {
-  name: 'app',
-};
+  data() {
+    return {
+      isCollapse: false
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
 </script>
 
 <style>

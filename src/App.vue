0c73406646328149ada5f47351cdfd18a8e0fc85
Leecason <leecason0722@gmail.com>
2017-08-19 17:01:09 +0800

    Modify navigator text

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
       <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" theme="dark">
         <el-menu-item index="1">
           <i class="el-icon-star-on"></i>
-          <span slot="title">导航一</span>
+          <span slot="title">Home</span>
         </el-menu-item>
         <el-menu-item index="2">
           <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
+          <span slot="title">Personal</span>
         </el-menu-item>
         <el-menu-item index="3">
           <i class="el-icon-setting"></i>
-          <span slot="title">导航三</span>
+          <span slot="title">Photos</span>
         </el-menu-item>
       </el-menu>
     </div>
